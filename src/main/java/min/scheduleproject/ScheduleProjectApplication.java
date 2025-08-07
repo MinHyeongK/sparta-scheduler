a3cef5bc131b71e21333c0e9df08cfcb9c2bf3f2
MinHyeongK <syjjmh1@naver.com>
2025-08-07 23:58:39 +0900

    chore: initialize project directory structure

---
+++ src/main/java/min/scheduleproject/ScheduleProjectApplication.java
@@ -0,0 +1,12 @@
+package min.scheduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectApplication.class, args);
+    }
+
+}
